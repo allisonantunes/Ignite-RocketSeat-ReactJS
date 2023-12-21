e6e3f0baaec849b7f16cc29a9c47d69ad0253e5d
allison <allisonabc@hotmail.com>
2023-12-21 17:36:38 -0300

    home

--- ignite-timer/src/@types/styles.d.ts
+++ ignite-timer/src/@types/styles.d.ts
@@ -1,11 +1,11 @@
 // esse arquivo usa para quando for usar os themas de variavel, aparecer onde esta usando. ctrl + espaço
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
 // guardando o defaulttheme no themeType.
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 // tipagem no styled components
 declare module 'styled-components' {
-    export interface defaultTheme extends ThemeType {}
+  export interface defaultTheme extends ThemeType {}
 }
