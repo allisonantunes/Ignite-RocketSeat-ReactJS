2380ad50ad1b964573b85112a4930fa8f97b8c68
Allison Antunes <allisonabc@hotmail.com>
2024-01-30 11:48:35 -0300

    dt-money

---
+++ ignite-Dt-Money/src/pages/Transactions/index.tsx
@@ -0,0 +1,36 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary"
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+    return (
+        <div>
+            <Header />
+            <Summary/>
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                        <td width='50%'>Desenvolvimento de site</td>
+                        <PriceHighLight variant="income">
+                        R$ 12.000,00
+                        </PriceHighLight>
+                        <td>Venda</td>
+                        <td>13/01/24</td>
+                        </tr>
+                        
+                        <tr>
+                        <td width='50%'>Desenvolvimento de site</td>
+                        <PriceHighLight variant="outcome">
+                        R$- 12.000,00
+                        </PriceHighLight>
+                        <td>Venda</td>
+                        <td>13/01/24</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+            
+        </div>
+    )
+}
