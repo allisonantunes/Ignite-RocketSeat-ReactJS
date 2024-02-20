2d59addf2a434d627aacdd8cddfc694388fd6e1c
allison <allisonabc@hotmail.com>
2024-02-20 17:13:01 -0300

    logoutDoUsuario

--- ignite-pizza-shop-web/pizza-shop-web/src/components/account-menu.tsx
+++ ignite-pizza-shop-web/pizza-shop-web/src/components/account-menu.tsx
@@ -8,8 +8,38 @@ import {
 } from '@radix-ui/react-dropdown-menu'
 import { Button } from './ui/button'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+import { Dialog } from './ui/dialog'
+import { DialogTrigger } from '@radix-ui/react-dialog'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { getProfile } from '@/api/get-profile'
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { Skeleton } from './ui/skeleton'
+import { StoreProfileDialog } from './store-profile-dialog'
+import { signOut } from '@/api/sign-out'
+import { useNavigate } from 'react-router-dom'
 
 export function AccountMenu() {
+  const navegate = useNavigate()
+
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ['managed-restaurant'],
+      queryFn: getManagedRestaurant,
+      staleTime: Infinity,
+    })
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navegate('/sign-in', { replace: true })
+    },
+  })
+
   return (
     <Dialog>
       <DropdownMenu>
@@ -18,15 +48,28 @@ export function AccountMenu() {
             variant="outline"
             className="flex select-none items-center gap-2"
           >
-            pizza shop
+            {isLoadingManagedRestaurant ? (
+              <Skeleton className="h-4 w-40" />
+            ) : (
+              managedRestaurant?.name
+            )}
             <ChevronDown className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
 
         <DropdownMenuContent align="end" className="w-56">
           <DropdownMenuLabel className="flex flex-col">
-            <span>allison</span>
-            <span>@email.com</span>
+            {isLoadingProfile ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span>{profile?.email}</span>
+              </>
+            )}
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
 
