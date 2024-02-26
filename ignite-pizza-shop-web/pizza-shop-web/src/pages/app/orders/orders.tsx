e56cb6ec5e9eb7d2ca1a8ec30d0ca136662c3e93
Allison Antunes <allisonabc@hotmail.com>
2024-02-26 17:16:38 -0300

    filters

--- ignite-pizza-shop-web/pizza-shop-web/src/pages/app/orders/orders.tsx
+++ ignite-pizza-shop-web/pizza-shop-web/src/pages/app/orders/orders.tsx
@@ -11,12 +11,38 @@ import { OrderTableFilter } from './order-table-filters'
 import { Pagination } from '@/components/pagination'
 import { useQuery } from '@tanstack/react-query'
 import { getOrders } from '@/api/get-orders'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const orderId = searchParams.get('orderId')
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
   const { data: result } = useQuery({
-    queryKey: ['orders'],
-    queryFn: getOrders,
+    queryKey: ['orders', pageIndex, orderId, customerName, status],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+        customerName,
+        orderId,
+        status: status === 'all' ? null : status,
+      }),
   })
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((statePrev) => {
+      statePrev.set('page', (pageIndex + 1).toString())
+      return statePrev
+    })
+  }
   return (
     <>
       <Helmet title="Pedidos" />
@@ -47,7 +73,14 @@ export function Orders() {
             </Table>
           </div>
 
-          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+          {result && (
+            <Pagination
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+            />
+          )}
         </div>
       </div>
     </>
