e8526a5bb9954f475d2b48c20832558bdc55a9c8
Allison t <allisonantunes62@gmail.com>
2024-04-08 08:29:27 -0300

    username

--- 06-ignite-next/ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ 06-ignite-next/ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -7,50 +7,51 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 
 const clainUsernameFormSchema = z.object({
-    username: z
-        .string()
-        .min(3, { message: 'Pelo menos 3 letras' })
-        .regex(/^([a-z\\-\\_]+)$/i, { message: 'Apenas letras e Underline' })
-        .transform((username) => username.toLowerCase()),
+  username: z
+    .string()
+    .min(3, { message: 'Pelo menos 3 letras' })
+    .regex(/^([a-z\\-\\_]+)$/i, { message: 'Apenas letras e Underline' })
+    .transform((username) => username.toLowerCase()),
 })
 
 type ClainUsernameForm = z.infer<typeof clainUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<ClainUsernameForm>({
-        resolver: zodResolver(clainUsernameFormSchema),
-    })
-    const router = useRouter()
-    async function handleClainUsernameForm(data: ClainUsernameForm) {
-        const { username } = data
-        await router.push()
-    }
-    return (
-        <>
-            <Form as="form" onSubmit={handleSubmit(handleClainUsernameForm)}>
-                <TextInput
-                    crossOrigin=""
-                    size="sm"
-                    prefix="ignite.com/"
-                    placeholder="seu-usuário"
-                    {...register('username')}
-                />
-                <Button size="sm" type="submit">
-                    Reservar
-                    <ArrowRight />
-                </Button>
-                <FormAnnotation>
-                    <Text size="sm">
-                        {errors.username
-                            ? errors.username.message
-                            : 'Digite o nome do Usuario desejado'}
-                    </Text>
-                </FormAnnotation>
-            </Form>
-        </>
-    )
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ClainUsernameForm>({
+    resolver: zodResolver(clainUsernameFormSchema),
+  })
+  const router = useRouter()
+
+  async function handleClainUsernameForm(data: ClainUsernameForm) {
+    const { username } = data
+    await router.push(`/register?username=${username}`)
+  }
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClainUsernameForm)}>
+        <TextInput
+          crossOrigin=""
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit" disabled={isSubmitting}>
+          Reservar
+          <ArrowRight />
+        </Button>
+        <FormAnnotation>
+          <Text size="sm">
+            {errors.username
+              ? errors.username.message
+              : 'Digite o nome do Usuario desejado'}
+          </Text>
+        </FormAnnotation>
+      </Form>
+    </>
+  )
 }
